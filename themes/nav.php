2d8b7ca8c191fb3b98a1eca32bb36c16a90ad122
LDLTC <97686498+GoldenCoqui@users.noreply.github.com>
2023-08-25 23:57:10 -0500

    Add files via upload

---
+++ themes/nav.php
@@ -0,0 +1,28 @@
+
+
+<!-- Use <"?"php include('themes/nav.php'); ?> -->
+
+<aside>
+        <ul class="tree-view">
+          <li>GitHub Page</li>
+          <li>
+            <a href="index.php">Home</a>
+            <ul>
+              <li><a href="blog.php">Blog</a></li>
+              <li><a href="work-exp.php">Work Experience</a></li>
+              <li><a href="college.php">College</a></li>
+              <li><a href="skills.php">Skills</a></li>
+              <li>
+                <a href="projects.php">Projects</a>
+                <ul>
+                  <li><a href="projects.php">Raspberry Pi Web Server</a></li>
+                  <li><a href="projects.php">MIPS R3000</a></li>
+                  <li><a href="projects.php">Java Pokergame</a></li>
+                </ul>
+              </li>
+              <li><a href="volunteer.php">Volunteer</a></li>
+              <li><a href="contact.php">Contact Info</a></li>
+            </ul>
+          </li>
+        </ul>
+    </aside>
---
+++ themes/under-construction.php
@@ -0,0 +1,32 @@
+
+<!-- USE  <"?"php include('themes/under-construction.php'); ?> -->
+
+
+<body>
+  <?php include('themes/nav.php'); ?>
+    <main>
+        <h1></h1>
+        <div class="window" style="width: 250px" padding="10px">       
+            <div class="title-bar">
+                <div class="title-bar-text">under-construction.exe</div>
+                    <div class="title-bar-controls">
+                        <button aria-label="Minimize"></button>
+                        <button aria-label="Maximize"></button>
+                        <button aria-label="Close"></button>
+                    </div>
+                </div>
+                <div class="window-body">
+                    <p><b>
+                        UNDER CONSTRUCTION
+                        <br><br>
+                        <img src="images/old-under-construction-gif.gif" alt="construction" width="150px" center>
+                    </b></p>
+                    <section class="field-row" style="justify-content: flex-end">
+                        <a href="index.php"><button>Ok</button></a>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
